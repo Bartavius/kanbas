e7dd32555323c67bbdb0d7ebcab7f5e06bc7bb97
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-09-14 21:49:55 -0400

    Finished all of exercise 3.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Labs from "./Labs";
+import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="Labs" />} />
+        <Route path="/" element={<Navigate to="Kanbas" />} />
         <Route path="/Labs/*" element={<Labs />} />
+        <Route path="/Kanbas/*" element={<Kanbas />} />
       </Routes>
     </HashRouter>
   );
