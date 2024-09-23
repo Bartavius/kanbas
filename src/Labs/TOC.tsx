3247d2b5b2a27e86b649a623d340e15c9d4d2b19
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-09-22 23:59:06 -0400

    made github href open a new tab

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,7 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-        <li><a href="https://github.com/Bartavius/cs4550" id="wd-github">GitHub</a></li>
+        <li><a href="https://github.com/Bartavius/cs4550" id="wd-github" target="_blank" rel="noreferrer">GitHub</a></li>
       </ul>
     );
   }
