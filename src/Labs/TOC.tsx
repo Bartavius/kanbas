5f0aa7e01db7b55cf6bcd56c976fa6568cc2575e
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-10-01 10:35:24 -0400

    up to 3.1.4

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -8,6 +8,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
+        <li><Link to="https://github.com/Bartavius/cs4550">github link</Link></li>
         <li><a href="https://github.com/Bartavius/cs4550" id="wd-github" target="_blank" rel="noreferrer">GitHub</a></li>
       </ul>
     );
