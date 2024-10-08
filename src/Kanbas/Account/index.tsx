a93dca6898f7adea4593f560f3810fff955c28da
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-10-08 15:13:35 -0400

    removed tables from account page

--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -6,21 +6,19 @@ import Signup from "./Signup";
 export default function Account() {
   return (
     <div id="wd-account-screen">
-      <table>
-        <tr>
-          <td valign="top">
-            <AccountNavigation />
-          </td>
-          <td valign="top">
-            <Routes>
-              <Route path="/"
-                     element={<Navigate to="/Kanbas/Account/Signin" />} />
-              <Route path="/Signin" element={<Signin />} />
-              <Route path="/Profile" element={<Profile />} />
-              <Route path="/Signup" element={<Signup />} />
-            </Routes>
-          </td>
-        </tr>
-      </table>
+      <div className="row">
+        <div className="col-1">
+          <AccountNavigation/>
+        </div>
+        <div className="col-4 wd-main-content-offset p-1">
+          <Routes>
+            <Route path="/"
+                  element={<Navigate to="/Kanbas/Account/Signin" />} />
+            <Route path="/Signin" element={<Signin />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/Signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </div>
     </div>
 );}
