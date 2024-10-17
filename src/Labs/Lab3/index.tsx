977f2d7cdab82fb88eadf0ec02567e1be4920821
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-10-16 23:02:50 -0400

    completed all of 2.4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -17,6 +17,13 @@ import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import FilterFunction from "./FilterFunction";
 import JsonStringify from "./JsonStringify";
+import House from "./House";
+import TodoItem from "./todos/TodoItem";
+import TodoList from "./todos/TodoList";
+import Spreading from "./Spreading";
+import Destructing from "./Destructing";
+import FunctionDestructing from "./FunctionDestructing";
+import DestructingImports from "./DestructingImports";
 
 export default function Lab3() {
     return (
@@ -41,6 +48,13 @@ export default function Lab3() {
             <FindIndex />
             <FilterFunction />
             <JsonStringify />
+            <House />
+            <TodoItem />
+            <TodoList />
+            <Spreading />
+            <Destructing />
+            <FunctionDestructing />
+            <DestructingImports />
         </div>
     );
 }
