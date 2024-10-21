04c677f9c851e56af77c6d4e41c5c598e2c6dad4
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-10-21 02:09:09 -0400

    refactored course navigation (3.5)

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,67 +1,24 @@
 import { Link } from "react-router-dom";
-import { useLocation } from "react-router";
+import { useLocation, useParams } from "react-router";
+import { courses } from "../Database";
 
 export default function CoursesNavigation() {
   const { pathname } = useLocation();
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
   return (
     <div id="wd-courses-navigation">
       <ul className="nav nav-pills fs-5 rounded-0 list-group">
-        <li className="nav-item">
+        {links.map((link) => 
+          <li className="nav-item">
           <Link
-            id="wd-course-home-link" to="/Kanbas/Courses/1234/Home"
-            className={`nav-link text-danger border-0 ${pathname === "/Kanbas/Courses/1234/Home" ? "active-secondary-nav" : ""}`}>
-            Home
+            id="wd-course-home-link" to={`/Kanbas/Courses/${course && course._id}/${link}`}
+            className={`nav-link text-danger border-0 ${pathname === `/Kanbas/Courses/${course && course._id}/${link}` ? "active-secondary-nav" : ""}`}>
+            {link}
           </Link>
         </li>
-        <li className="nav-item">
-          <Link
-            id="wd-course-modules-link" to="/Kanbas/Courses/1234/Modules"
-            className={`nav-link text-danger border-0 ${pathname.includes("Modules") ? "active-secondary-nav" : ""}`}>
-            Modules
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            id="wd-course-piazza-link" to="/Kanbas/Courses/1234/Piazza"
-            className={`nav-link text-danger border-0 ${pathname.includes("Piazza") ? "active-secondary-nav" : ""}`}>
-            Piazza
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            id="wd-course-zoom-link" to="/Kanbas/Courses/1234/Zoom"
-            className={`nav-link text-danger border-0 ${pathname.includes("Zoom") ? "active-secondary-nav" : ""}`}>
-            Zoom
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            id="wd-course-quizzes-link" to="/Kanbas/Courses/1234/Assignments"
-            className={`nav-link text-danger border-0 ${pathname.includes("Assignments") ? "active-secondary-nav" : ""}`}>
-            Assignments
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            id="wd-course-assignments-link" to="/Kanbas/Courses/1234/Quizzes"
-            className={`nav-link text-danger border-0 ${pathname.includes("Quizzes") ? "active-secondary-nav" : ""}`}>
-            Quizzes
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            id="wd-course-grades-link" to="/Kanbas/Courses/1234/Grades"
-            className={`nav-link text-danger border-0 ${pathname.includes("Grades") ? "active-secondary-nav" : ""}`} >
-            Grades
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link
-            id="wd-course-people-link" to="/Kanbas/Courses/1234/People"
-            className={`nav-link text-danger border-0 ${pathname.includes("People") ? "active-secondary-nav" : ""}`}>
-            People
-          </Link>
-        </li>
+        )}
       </ul>
     </div>
   );
