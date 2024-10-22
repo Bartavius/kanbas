a125068bf9dd21adf6f5fc3896b398bfa5a46514
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-10-21 20:34:14 -0400

    created a data-driven assignments page, and generated a bunch of assignments for the database with GPT

--- src/Kanbas/Courses/Assignments/index.tsx
+++ src/Kanbas/Courses/Assignments/index.tsx
@@ -4,8 +4,13 @@ import { GoTriangleDown } from "react-icons/go";
 import SectionControlButton from "./SectionControlButton";
 import { GrNotes } from "react-icons/gr";
 import LessonControlButtons from "../Modules/LessonControlButtons";
+import { useParams } from "react-router";
+import * as db from "../../Database";
 
 export default function Assignments() {
+    const { cid } = useParams();
+    const assignments = db.assignments;
+    let assignmentNum = 0;
     return (
       <div>
         {/* assignment top */}
@@ -19,27 +24,29 @@ export default function Assignments() {
 
                 <div className="col-8 d-flex justify-content-end align-text-end">
                   <button className="btn btn-danger text-white rounded-1 me-1">
-                  <BsPlus className="text-white"/>Assignment</button>
+                  <BsPlus className="text-white"/>Assignment</button> 
                   <button className="btn btn-secondary text-dark rounded-1 me-1">
                   <BsPlus />Group</button>
                 </div>
               </div>
 
-
-
-        <ul id="wd-assignments-section" className="list-group rounded-0">
           {/* assignment section title */}
           <li className="wd-assignment-list list-group-item p-0 mb-5 fs-5 border-gray">
             <div className="wd-assignment-title p-3 ps-2 bg-secondary">
               <BsGripVertical className="me-2 fs-3"/>
               <GoTriangleDown />
-              ASSIGNMENTS
+              ASSIGNMENTS {/* currently no directions on if we'll format assignment groups, so that will be tbd */}
               <SectionControlButton />
             </div>
 
             {/* assignments list */}
+
             <ul className="wd-assignments list-group rounded-0">
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1">
+              {assignments
+                .filter((assignment: any) => assignment.course === cid)
+                .map((assignment: any) => (
+
+                  <li className="wd-assignment-list-item list-group-item p-3 ps-1">
                   <div className="row">
                     <div className="col-2">
                       <BsGripVertical className="me-2 fs-3" />
@@ -48,9 +55,10 @@ export default function Assignments() {
                     <div className="col-8 text-start">
                       <a className="wd-assignment-link" 
                         href="#/Kanbas/Courses/1234/Assignments/123">
-                        A1 - ENV + HTML
+                        A{++assignmentNum} - {assignment.title}
                       </a>
                       <p>Multiple Modules | <b>Not available until</b> May 6 at 12:00am | <b>Due</b> May 13 at 11:59pm | 100pts</p>
+                      {/* Haven't been given directions on how this part is formatted, so this will be left static until further notice also */}
                     </div>
                     <div className="col-2">
                       <LessonControlButtons />
@@ -58,46 +66,11 @@ export default function Assignments() {
                   </div>
                 </li>
 
-
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1">
-                  <div className="row">
-                    <div className="col-2">
-                      <BsGripVertical className="me-2 fs-3" />
-                      <GrNotes className="me-2 fs-3 text-success" />
-                    </div>
-                    <div className="col-8">
-                      <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/124">
-                      A2 - CSS + BOOTSTRAP
-                      </a>
-                      <p>Multiple Modules | <b>Not available until</b> May 13 at 12:00am | <b>Due</b> May 20 at 11:59pm | 100pts</p>
-                    </div>
-                    <div className="col-2">
-                      <LessonControlButtons />
-                    </div>
-                  </div>
-                </li>
-
-                <li className="wd-assignment-list-item list-group-item p-3 ps-1">
-                  <div className="row">
-                    <div className="col-2">
-                      <BsGripVertical className="me-2 fs-3" />
-                      <GrNotes className="me-2 fs-3 text-success" />
-                    </div>
-                    <div className="col-8">
-                      <a className="wd-assignment-link" href="#/Kanbas/Courses/1234/Assignments/125">
-                      A3 - JAVASCRIPT + REACT
-                      </a>
-                      <p>Multiple Modules | <b>Not available until</b> May 20 at 12:00am | <b>Due</b> May 27 at 11:59pm | 100pts</p>
-                    </div>
-                    <div className="col-2">
-                      <LessonControlButtons />
-                    </div> 
-                  </div>
-                </li>
+                ))
+              }
             </ul>
           </li>
-        </ul>
-      </div>
-  );
-}
-  
+        </div>
+    )
+  }
+        
