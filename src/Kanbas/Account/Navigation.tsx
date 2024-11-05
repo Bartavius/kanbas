5e1575a18425976ca99add97e023d6357ca2e79e
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-11-04 23:15:04 -0500

    hid profile page from unsigned users, and also change redirection to profile when signed in. hid signin and signup when a user is logged in also

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
+import { useSelector } from "react-redux";
 export default function AccountNavigation() {
   const { pathname } = useLocation();
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  // " Reimplement the Account Navigation sidebar so that it hides the Signin and Signup navigation links
+  // if a user is already signed in, and hides the Profile link if a user is not yet signed in.
+
   return (
     <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
       <ul className="nav nav-pills fs-5 rounded-0 list-group">
-      <li className="nav-item">
-      <Link to={"/Kanbas/Account/Signin"}  className={`nav-link text-danger border-0 mb-3 pl-2 ${pathname.includes("Signin") ? "active-secondary-nav" : ""}`}> Signin  </Link>
-      </li>
-      
-      <li className="nav-item">
-        <Link to={"/Kanbas/Account/Signup"}  className={`nav-link text-danger border-0 mb-3 pl-2 ${pathname.includes("Signup") ? "active-secondary-nav" : ""}`}> Signup  </Link>
-      </li>
-      <li className="nav-item">
-      <Link to={"/Kanbas/Account/Profile"} className={`nav-link text-danger border-0 mb-3 pl-2 ${pathname.includes("Profile") ? "active-secondary-nav" : ""}`}> Profile </Link>
-    </li></ul></div>
+        {links.map( ( link: string ) => (
+          <li className="nav-item">
+          <Link to={`/Kanbas/Account/${link}`}  className={`nav-link text-danger border-0 mb-3 pl-2 ${pathname.includes(link) ? "active-secondary-nav" : ""}`}> {link}  </Link>
+          </li>
+        ))}</ul></div>
 );}
--- src/Kanbas/Account/index.tsx
+++ src/Kanbas/Account/index.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from "react-router";
+import { useSelector } from "react-redux";
 import AccountNavigation from "./Navigation";
 import Profile from "./Profile";
 import Signin from "./Signin";
 import Signup from "./Signup";
 export default function Account() {
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
   return (
     <div id="wd-account-screen">
       <div className="row">
@@ -13,7 +15,7 @@ export default function Account() {
         <div className="col-6 wd-main-content-offset p-1">
           <Routes>
             <Route path="/"
-                  element={<Navigate to="/Kanbas/Account/Signin" />} />
+                  element={<Navigate to = {currentUser ? "/Kanbas/Account/Profile" : "/Kanbas/Account/Signin"} /> } />
             <Route path="/Signin" element={<Signin />} />
             <Route path="/Profile" element={<Profile />} />
             <Route path="/Signup" element={<Signup />} />
