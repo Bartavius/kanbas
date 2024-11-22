27a4ecdf909d592aa68c00cc6fabb748b1ba5fe2
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-11-21 19:18:19 -0500

    3.6 completed

--- src/Labs/Lab5/client.ts
+++ src/Labs/Lab5/client.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
+
 export const fetchWelcomeMessage = async () => {
   const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
   return response.data;
@@ -20,17 +21,14 @@ export const fetchTodos = async () => {
   const response = await axios.get(TODOS_API);
   return response.data;
 };
-
 export const removeTodo = async (todo: any) => {
     const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
     return response.data;
-  };
+};
 export const deleteTodo = async (todo: any) => {
     const response = await axios.delete(`${TODOS_API}/${todo.id}`);
     return response.data;
-  };
-  
-
+};
 export const createTodo = async () => {
     const response = await axios.get(`${TODOS_API}/create`);
     return response.data;
@@ -38,6 +36,11 @@ export const createTodo = async () => {
 export const postTodo = async (todo: any) => {
     const response = await axios.post(`${TODOS_API}`, todo);
     return response.data;
-  };
+};
+export const updateTodo = async (todo: any) => {
+  const response = await axios.put(`${TODOS_API}/${todo.id}`, todo);
+  return response.data;
+};
+
   
 
