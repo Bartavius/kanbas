9f291302f492f6cd6f8eb200bb60bb85d4e5a6ac
Jirath Lojanarungsiri <lojanarungsiri.j@northeastern.edu>
2024-12-11 18:34:02 -0500

    completed quiz display

--- src/Kanbas/Courses/Quizzes/client.ts
+++ src/Kanbas/Courses/Quizzes/client.ts
@@ -4,13 +4,29 @@ const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
 const axiosWithCredentials = axios.create({ withCredentials: true });
 
-
 export const getQuizzesFromCourse = async (cid: string) => {
-    const { data } = await axiosWithCredentials.get(`${QUIZZES_API}/${cid}`);
-    return data
-}
+  const { data } = await axiosWithCredentials.get(`${QUIZZES_API}/${cid}`);
+  return data;
+};
 
 export const addQuizToCourse = async (cid: String) => {
-    const {data } = await axiosWithCredentials.post(`${QUIZZES_API}/${cid}`);
+  const { data } = await axiosWithCredentials.post(`${QUIZZES_API}/${cid}`);
+  return data;
+};
+
+export const deleteQuiz = async (qid: String) => {
+  const { data } = await axiosWithCredentials.delete(
+    `${QUIZZES_API}/${qid}`
+  );
+  return data;
+};
+
+export const updateQuiz = async (qid: String, newUpdate: any) => {
+    const { data } = await axiosWithCredentials.put(`${QUIZZES_API}/${qid}`, newUpdate);
     return data;
-}
+  };
+
+  export const getLastAttempt = async (uid: String, qid: string) => {
+    const { data } = await axiosWithCredentials.get(`${QUIZZES_API}/${uid}/${qid}`);
+    return data;
+  }
--- src/Kanbas/Courses/Quizzes/index.tsx
+++ src/Kanbas/Courses/Quizzes/index.tsx
@@ -6,7 +6,11 @@ import { BsGripVertical } from "react-icons/bs";
 import { GoTriangleDown } from "react-icons/go";
 import SectionControlButton from "../Assignments/SectionControlButton";
 import { Link } from "react-router-dom";
-import { FaPlus } from "react-icons/fa";
+import { FaCheckCircle, FaPlus, FaTrash } from "react-icons/fa";
+import { IoEllipsisVertical } from "react-icons/io5";
+import { FaPencil } from "react-icons/fa6";
+import { SlRocket } from "react-icons/sl";
+import { ImCross } from "react-icons/im";
 
 export default function Quizzes() {
   const { cid } = useParams();
@@ -15,21 +19,37 @@ export default function Quizzes() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const [loading, setLoading] = useState<boolean>(true);
   const [reload, setReload] = useState<boolean>(false);
+  const [menuVisible, setMenuVisible] = useState<string | null>(null);
+  const [name, setName] = useState("");
+  const [allQuizzes, setAllQuizzes] = useState<any>([]);
+  const handleContextMenu = (quizId: string) => {
+    setMenuVisible(menuVisible === quizId ? null : quizId);
+  };
 
   // TODO: if faculty / admin, can edit quizzes
 
-  const fetchQuizzes = async () => {
+  const fetchAllQuizzes = async () => {
     if (!cid) return;
     try {
       const loadedQuizzes = await quizClient.getQuizzesFromCourse(cid);
+      setAllQuizzes(loadedQuizzes);
       setQuizzes(loadedQuizzes);
-      console.log(quizzes);
     } catch (error) {
       console.error(error);
     }
     setLoading(false);
   };
 
+  const fetchQuizzes = () => {
+      const filteredQuizzes = allQuizzes.filter((quiz: any) => {
+        const nameMatch = name
+          ? quiz.title.toLowerCase().includes(name.toLowerCase())
+          : true;
+        return nameMatch;
+      });
+      setQuizzes(filteredQuizzes);
+    }
+  
   const createQuiz = async () => {
     if (!cid) return;
     try {
@@ -40,9 +60,51 @@ export default function Quizzes() {
     }
   };
 
+  const deleteQuiz = async (qid: String) => {
+    try {
+      await quizClient.deleteQuiz(qid);
+      setMenuVisible(null);
+      setReload(!reload);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const publishQuiz = async (qid: String, newPublished: boolean) => {
+    try {
+      await quizClient.updateQuiz(qid, { published: newPublished });
+      setMenuVisible(null);
+      setReload(!reload);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const displayAvailability = (quiz: any) => {
+    const today = new Date();
+    if (quiz.available_from <= today && quiz.available_until >= today) {
+      return "Available";
+    } else if (quiz.available_from > today) {
+      return `Not Available until ${quiz.available_from.toString()}`;
+    } else {
+        return "Closed";
+      }
+  }
+
+  const getScore = async (qid: string) => {
+    const last =  await quizClient.getLastAttempt(currentUser._id, qid);
+    const score = last.responses.filter((question: any) => question.isCorrect === true).length;
+    return score;
+  }
+
+
+  useEffect(() => {
+    fetchAllQuizzes();
+  }, [cid, reload]);
+
   useEffect(() => {
     fetchQuizzes();
-  }, [cid]);
+  }, [name]);
 
   return (
     <div>
@@ -50,61 +112,112 @@ export default function Quizzes() {
         <h5>...Loading</h5>
       ) : (
         <div id="wd-quizzes-list">
-              {/* <h3>ADD SEARCH AND +QUIZ BUTTON HERE. THE SEARCH SHOULD BE FULLY
-              FUNCTIONAL AS A NAME FILTER</h3> */}
-              <input
-                // onChange={(e) => setName(e.target.value)}
-                placeholder="Search quiz"
-                className="form-control float-start w-25 me-2 wd-filter-by-name"
-              />
-              <button
-              onClick={createQuiz}
-              className="float-end btn btn-danger wd-add-quiz mb-1 me-1"
-            >
-              <FaPlus className="me-2" /> Quiz
-            </button>
-            <br />
-            <br />
-                
-    
-              <ul className="list-group-item p-0 mb-5 fs-5 border-gray">
-          <div className="list-group-item">
-            <div className="wd-quiz-title p-3 ps-2 bg-secondary">
-              {(currentUser.role === "FACULTY" ||
-                currentUser.role === "ADMIN") && (
-                <BsGripVertical className="me-2 fs-3" />
-              )}
-              <GoTriangleDown />
-              QUIZZES
-              <SectionControlButton percent="100" />
-              {/* MIGHT HAVE TO CHANGE THIS */}
+          <input
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Search quiz"
+            className="form-control float-start w-25 me-2 wd-filter-by-name"
+          />
+          <button
+            onClick={createQuiz}
+            className="float-end btn btn-danger wd-add-quiz mb-1 me-1"
+          >
+            <FaPlus className="me-2" /> Quiz
+          </button>
+          <br />
+          <br />
+
+          <ul className="list-group-item p-0 mb-5 fs-5 border-gray">
+            <div className="list-group-item">
+              <div className="wd-quiz-title p-3 ps-2 bg-secondary">
+                {(currentUser.role === "FACULTY" ||
+                  currentUser.role === "ADMIN") && (
+                  <BsGripVertical className="me-2 fs-3" />
+                )}
+                <GoTriangleDown />
+                QUIZZES
+                <SectionControlButton percent="100" />
+                {/* MIGHT HAVE TO CHANGE THIS */}
+              </div>
+              <ul className="wd-quiz-list-items list-group">
+                {quizzes.map((quiz: any) => (
+                  <li
+                    className="wd-quiz-list-item list-group-item list-group-item p-3 ps-1 rounded-0"
+                    key={quiz._id}
+                  >
+                    <div className="row align-items-center">
+                      <div className="col-2 wd-quiz-left-section-control justify-content-center">
+
+                      <SlRocket className="text-success fs-2 ms-3"/>
+
+                      </div>
+                      <div className="col-8 text-start">
+                        <Link to={`${quiz._id}`}>{quiz.title}</Link>
+                        <p>
+                          <b> {displayAvailability(quiz)} </b> | <b> Due </b> {quiz.due_date} | {quiz.points} pts | {quiz.questions.length} Questions
+                          {currentUser.role==="STUDENT" ? `Score ${getScore(quiz._id)}` : ""}
+                        </p>
+                      </div>
+                      <div className="col-2">
+
+                      
+                        <IoEllipsisVertical
+                          className="fs-3 float-end me-3"
+                          onClick={() => handleContextMenu(quiz._id)}
+                        />
+                        {quiz.published ? 
+                            < FaCheckCircle className="text-success fs-3 float-end me-4"/> :
+                                  <ImCross className="text-danger fs-3 float-end me-4" />
+                                
+                              }
+                        {menuVisible === quiz._id && (
+                          <div
+                            className="dropdown-menu show"
+                            style={{ position: "absolute" }}
+                          >
+                            <button
+                              className="dropdown-item"
+                              onClick={() => navigate(`${quiz._id}/Editor`)}
+                            >
+                              <FaPencil className="text-primary" /> Edit
+                            </button>
+                            <button
+                              className="dropdown-item"
+                              onClick={() => deleteQuiz(quiz._id)}
+                            >
+                              <FaTrash className="text-danger" /> Delete
+                            </button>
+                            <button
+                              className="dropdown-item"
+                              onClick={() =>
+                                publishQuiz(quiz._id, !quiz.published)
+                              }
+                            >
+                              {quiz.published ? (
+                                <span>
+                                  <ImCross className="text-danger" />
+                                  {" "}Unpublish
+                                </span>
+                              ) : (
+                                <span>
+                                  < FaCheckCircle className="text-success"/>
+                                  {" "}Publish
+                                </span>
+                              )}
+                            </button>
+                            <button
+                              className="dropdown-item"
+                              onClick={() => setMenuVisible(null)}
+                            >
+                              Cancel
+                            </button>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
             </div>
-            <ul className="list-group">
-              {quizzes.map((quiz: any) => (
-                <li className="list-group-item list-group-item p-3 ps-1 rounded-0">
-                  <div className="row align-items-center">
-                    <div className="col-2">
-                      {/* SECTION BUTTONS GO RIGHT HERE*/}
-                    </div>
-                    <div className="col-8 text-start">
-                      <Link to={`${quiz._id}`}>{quiz.title}</Link>
-                      <p>
-                        NEED TO ADD OTHER DETAILS LIKE QUIZ TAKEN, GRADE ETC.
-                        RIGHT IN THIS DIV. MAKE SURE TO ADD SECTION BUTTONS,
-                        FILTERS, EDIT BUTTONS, PUBLISH BUTTON, ETC.
-                      </p>
-                    </div>
-                    <div className="col-2">
-                      <p>
-                        CONTROL BUTTONS RIGHT HERE. EDIT, PUBLISH, DELETE, AND
-                        THREE DOTS SHOULD BE HERE
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
           </ul>
         </div>
       )}
